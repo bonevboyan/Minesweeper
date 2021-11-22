0b909cca0867efac352d0d56053d1af6677920e6
Blago Gunev <bgunev@gmail.com>
2021-11-22 17:26:59 +0200

    Implement play function

---
+++ src/game/objects/Coordinate.java
@@ -0,0 +1,13 @@
+package game.objects;
+
+public class Coordinate {
+
+    public int y, x;
+
+    public Coordinate(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+
+    public Coordinate() { }
+}
--- src/game/objects/Field.java
+++ src/game/objects/Field.java
@@ -4,12 +4,18 @@ public class Field {
     private final int HEIGHT = 10;
     private final int WIDTH = 15;
     private final int MIN_BOMB_COUNT = 1;
-    private final int MAX_BOMB_COUNT = 150;
+    private final int MAX_BOMB_COUNT = HEIGHT * WIDTH;
 
+    private final Coordinate[] adjacents = {
+            new Coordinate(1, 0), new Coordinate(1, 1), new Coordinate(0, 1),
+            new Coordinate(-1, 1), new Coordinate(-1, 0), new Coordinate(-1, -1),
+            new Coordinate(0, -1), new Coordinate(1, -1)
+    };
 
     private Cell[][] cells;
     private int bombCount = 10;
-    private boolean hasEnded = false;
+    private boolean hasLost = false;
+    private int closedCells = HEIGHT * WIDTH;
 
     public Field() {
         initializeBoard();
@@ -44,8 +50,12 @@ public class Field {
         }
     }
 
-    public boolean isHasEnded() {
-        return hasEnded;
+    public boolean hasLost() {
+        return hasLost;
+    }
+
+    public boolean hasWon() {
+        return closedCells == bombCount;
     }
 
     public void initializeBoard() {
@@ -68,15 +78,77 @@ public class Field {
         }
     }
 
-    public void play(int y, int x) throws IllegalArgumentException{
-        if (0 > y || y > HEIGHT || 0 > x || x > WIDTH) {
+    public void play(int y, int x) throws IllegalArgumentException, TypeNotPresentException{
+        if (!isInHeightLimits(y) || !isInWidthLimits(x)) {
             throw new IllegalArgumentException("Cell non-existent");
         }
 
-        if (cells[y][x].isFlagged || cells[y][x].isOpened) {
+        if (cells[y][x].isFlagged) {
+            throw new IllegalArgumentException("Trying to open a flagged cell");
+        }
+
+        if (cells[y][x] instanceof BombCell) {
+            hasLost = true;
+
+            for (int h = 0; h < HEIGHT; h++) {
+                for (int w = 0; w < WIDTH; w++) {
+                    cells[h][w].isOpened = true;
+                }
+            }
+
             return;
         }
 
+        if (!(cells[y][x] instanceof ClearCell currCell)) {
+            throw new TypeNotPresentException("Internal error. Cell of unrecognized type", new Throwable());
+        }
 
+        if (!currCell.isOpened) {
+            currCell.isOpened = true;
+            closedCells--;
+
+            if (currCell.getAdjacentBombs() == 0) {
+                openAllAdj(y, x);
+            }
+
+        } else {
+            if (getAdjacentFlagsCount(y, x) != currCell.getAdjacentBombs()) {
+                return;
+            }
+
+            openAllAdj(y, x);
+        }
     }
+
+    private void openAllAdj(int y, int x) {
+        for (Coordinate coord : adjacents) {
+            if (isInHeightLimits(y + coord.y) && isInWidthLimits(x + coord.x)
+                    && !cells[y + coord.y][x + coord.x].isFlagged
+                    && !cells[y + coord.y][x + coord.x].isOpened) {
+                play(y + coord.y, x + coord.x);
+            }
+        }
+    }
+
+    private boolean isInHeightLimits(int height) {
+        return height < HEIGHT && height >= 0;
+    }
+
+    private boolean isInWidthLimits(int width) {
+        return width < WIDTH && width >= 0;
+    }
+
+    private int getAdjacentFlagsCount(int y, int x) {
+        int adjFlags = 0;
+
+        for (Coordinate coord : adjacents) {
+            if (isInHeightLimits(y + coord.y) && isInWidthLimits(x + coord.x)
+                    && cells[y + coord.y][x + coord.x].isFlagged) {
+                adjFlags++;
+            }
+        }
+
+        return adjFlags;
+    }
+
 }
