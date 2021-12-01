5a04157f8523bee35bac32a4121cb62a34187e94
bonevboyan <boyanbonev43@gmail.com>
2021-12-01 14:07:12 +0200

    Update Data.java

--- src/game/data/Data.java
+++ src/game/data/Data.java
@@ -21,28 +21,28 @@ public class Data {
     public List<Record> getAllRecords(int difficulty) throws Exception {
         String data = api.sendGET(endpoints[difficulty]);
 
-        List <Record> records = parseJSONtoUsers(data);
+        List <Record> records = parseJSONtoRecord(data);
 
-        return orderUsersDescending(records);
+        return orderRecordsDescending(records);
     }
 
     public List<Record> getOwnRecords(String user, int difficulty) throws Exception {
         String data = api.sendGET(endpoints[difficulty]);
 
-        List <Record> records = parseJSONtoUsers(data);
+        List <Record> records = parseJSONtoRecord(data);
         records = records.stream().filter(u -> user.equals(u.user)).toList();
 
-        return orderUsersDescending(records);
+        return orderRecordsDescending(records);
     }
 
     public void postRecord(String name, long seconds, int difficulty) throws IOException {
         Record record = new Record(name, seconds);
-        String parsedUser = parseUserToJSON(record);
+        String parsedUser = parseRecordToJSON(record);
 
         api.sendPOST(endpoints[difficulty], parsedUser);
     }
 
-    private List<Record> parseJSONtoUsers(String json){
+    private List<Record> parseJSONtoRecord(String json){
         return Arrays.stream(json.split("},")).map(x -> {
             x = x.replaceAll("[{}\"]", "").replaceAll(",", ":");
             List<String> fields = Arrays.stream(x.split(":")).toList();
@@ -50,11 +50,11 @@ public class Data {
         }).toList();
     }
 
-    private String parseUserToJSON(Record record){
+    private String parseRecordToJSON(Record record){
         return String.format("{\"time\": \"%d\", \"user\": \"%s\"}", record.time, record.user);
     }
 
-    private List<Record> orderUsersDescending(List<Record> records){
+    private List<Record> orderRecordsDescending(List<Record> records){
         Record[] usersArr = new Record[records.size()];
         usersArr = records.toArray(usersArr);
         Arrays.sort(usersArr);
